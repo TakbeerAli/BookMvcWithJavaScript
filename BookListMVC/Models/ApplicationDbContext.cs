11a337f72bed0cf945cf8483c2fb0ecf98e2acb1
TakbeerAli <Takbeeralikhan@gmail.com>
2020-09-12 20:51:24 +0500

    MID-2 Project

---
+++ BookListMVC/Models/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookListMVC.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        //this is constructor of this class
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+            // this is for to set or create Book class model database in database
+       public DbSet<Book> Books { set; get; }
+    }
+}
